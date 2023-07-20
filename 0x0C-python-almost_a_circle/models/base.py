1b99da8c374024b41ad9f950cfe3898f3ee98cee
imelda-1 <im.ishimwe@gmail.com>
2023-07-20 13:10:18 -0700

    almost a cirlce

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionari
